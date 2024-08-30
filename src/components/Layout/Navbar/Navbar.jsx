01ea0d8466d56cd509d2a1227e5f2ff94c662857
Abdulrazzak-71 <devabdulrazzak31@gmail.com>
2024-08-30 19:26:39 +0530

    NAMING CHANFE

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loader from '../../Loader/loader';
 
 // import './navbar.css'
 
@@ -7,8 +8,9 @@ export default function Navbar() {
     <div className='absolute w-full border-t-4 border-orange-500 bg-white '>
       <div className='border-b border-gray-300'>
         <div className='flex max-w-6xl mx-auto'>
-          <span className='py-4 px-4 text-2xl font-semibold text-orange-500'>
-            POSTMAN CLONE
+            <Loader/>
+          <span className='py-4 px-4 text-2xl font-semibold text-orange-500 flex justify-center'>
+            PICKUP-BIZ POSTMAN
           </span>
         </div>
       </div>
